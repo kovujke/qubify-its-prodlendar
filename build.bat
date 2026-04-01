e45b95307c5b7dc2ba3727b4f4f69d2837c9adcc
Konstantin Vujnovic <tin.win.wien@gmail.com>
2026-04-01 18:00:08 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -76,47 +76,4 @@ ren "dist\calendar_app.exe" "%EXE_NAME%"
 echo.
 echo Build completed successfully.
 echo.
-
-REM startup
-set /p STARTUP=Start %APP_NAME% on Windows startup? (yes/no): 
-
-IF /I "%STARTUP%" NEQ "yes" (
-    echo Startup disabled.
-    pause
-    exit /b
-)
-
-echo Enabling startup...
-
-REM create shortcut
-set "EXE_PATH=%CD%\%DIST_DIR%\%EXE_NAME%"
-set "STARTUP_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "LNK_PATH=%STARTUP_DIR%\%EXE_NAME%.lnk"
-set "PS_FILE=%TEMP%\create_startup_shortcut.ps1"
-
-REM make sure startup folder exists
-if not exist "%STARTUP_DIR%" mkdir "%STARTUP_DIR%"
-
-REM create powershell script
-(
-echo $w = New-Object -ComObject WScript.Shell
-echo $s = $w.CreateShortcut("%LNK_PATH%")
-echo $s.TargetPath = "%EXE_PATH%"
-echo $s.WorkingDirectory = "%CD%\%DIST_DIR%"
-echo $s.Save()
-) > "%PS_FILE%"
-
-REM run powershell
-powershell -NoProfile -ExecutionPolicy Bypass -File "%PS_FILE%"
-
-REM cleanup
-del "%PS_FILE%" >nul 2>&1
-
-IF EXIST "%LNK_PATH%" (
-    echo Startup enabled successfully.
-) ELSE (
-    echo ERROR: Failed to create startup shortcut.
-)
-
-echo.
 pause
